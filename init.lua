ac1e4f362e911c6db3eba4d93c151cf745cce34f
Adamniels <adamnielsen401@gmail.com>
2026-05-13 19:11:29 +0200

    added more settings

---
+++ init.lua
@@ -0,0 +1,37 @@
+-- Basic editor settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.clipboard = "unnamedplus"
+
+-- Netrw explorer settings
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 0
+vim.g.netrw_altv = 1
+vim.g.netrw_winsize = 25
+vim.g.netrw_preview = 1
+
+-- Simple keymaps
+vim.keymap.set("n", "<F13>w", ":w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<F13>q", ":q<CR>", { desc = "Quit" })
+vim.keymap.set("n", "<F13>h", ":nohlsearch<CR>", { desc = "Clear search highlight" })
+
+-- Colorscheme
+vim.cmd.colorscheme("habamax")
+
+-- Load lazy.nvim
+require("config.lazy")
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+
+    config = function()
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+          },
+        },
+      })
+
+      vim.lsp.config("ts_ls", {})
+
+      vim.lsp.enable({
+        "lua_ls",
+        "ts_ls",
+      })
+
+      -- LSP keymaps
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover)
+
+      vim.keymap.set("n", "<F13>rn", vim.lsp.buf.rename)
+      vim.keymap.set("n", "<F13>ca", vim.lsp.buf.code_action)
+      vim.keymap.set("n", "<F13>f", vim.lsp.buf.format)
+
+      -- Diagnostics
+      vim.keymap.set("n", "[d", function()
+        vim.diagnostic.jump({ count = -1, float = true })
+      end)
+
+      vim.keymap.set("n", "]d", function()
+        vim.diagnostic.jump({ count = 1, float = true })
+      end)
+
+      vim.keymap.set("n", "<F13>e", vim.diagnostic.open_float)
+    end,
+  },
+}
