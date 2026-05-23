4793f500e3a12d56cccc73db33270fd06e79c125
Adamniels <adamnielsen401@gmail.com>
2026-05-23 12:49:32 +0200

    changed keybind

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -50,7 +50,7 @@ return {
       })
 
 
-      vim.keymap.set("n", "<F13>p", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<F13>a", builtin.find_files, { desc = "Find files" })
       vim.keymap.set("n", "<F13>g", builtin.live_grep, { desc = "Search text" })
       vim.keymap.set("n", "<F13>b", builtin.buffers, { desc = "Find buffers" })
       vim.keymap.set("n", "<F13>h", builtin.help_tags, { desc = "Help tags" })
